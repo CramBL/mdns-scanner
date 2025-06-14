d29ca756a408af0dc512eda720eb677208852610
Marc BK <72197092+CramBL@users.noreply.github.com>
2025-06-15 01:51:40 +0200

    use jemalloc on most unix and unix-like platforms (#58)
    * use jemalloc on most unix and unix-like platforms

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 ## [unreleased]
 
+## [0.11.1] - 2025-06-15
+
+### Changed
+
+- Use `jemalloc` on most unix and unix-like systems (using `mimalloc` on windows).
+
 ## [0.11.0] - 2025-06-14
 
 ### Added
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
+#[cfg(target_os = "windows")]
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
+#[cfg(all(
+    not(target_os = "windows"),
+    not(target_os = "openbsd"),
+    not(target_os = "freebsd"),
+    any(
+        target_arch = "x86_64",
+        target_arch = "aarch64",
+        target_arch = "powerpc64"
+    )
+))]
+#[global_allocator]
+static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+
 use std::sync::OnceLock;
 
 use semver::Version;
-
-// Don't enable on linux due to linker error
-#[cfg(not(target_os = "linux"))]
-#[global_allocator]
-static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 pub const APP_VERSION_MAJOR: &str = env!("CARGO_PKG_VERSION_MAJOR");
 pub const APP_VERSION_MINOR: &str = env!("CARGO_PKG_VERSION_MINOR");
