2dc38a29373d26ac1faf87b4a8884e0ee6300de6
Marc Beck König <mbkj@tutamail.com>
2025-07-21 13:24:21 +0200

    fix links in doc comments

--- crates/mds-util/src/constants.rs
+++ crates/mds-util/src/constants.rs
@@ -3,7 +3,7 @@ use std::net::{Ipv4Addr, SocketAddrV4};
 pub const MULTICAST_PORT: u16 = 5353;
 pub const MULTICAST_ADDR: Ipv4Addr = Ipv4Addr::new(224, 0, 0, 251);
 /// In multicast query messages, the Query Identifier SHOULD be set to zero on transmission
-/// https://www.rfc-editor.org/rfc/rfc6762.html
+/// <https://www.rfc-editor.org/rfc/rfc6762.html>
 pub const MDNS_QUERY_ID: u16 = 0;
 pub const MDNS_SOCKET_ADDR: SocketAddrV4 = SocketAddrV4::new(MULTICAST_ADDR, MULTICAST_PORT);
 
