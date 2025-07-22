f57118e5e20c71a7d7973fc8acf5e13f031308db
Marc BK <72197092+CramBL@users.noreply.github.com>
2025-07-22 11:26:05 +0200

    Add more TUI snapshot tests (#115)
    * add bin_size.txt to gitignore (file where the binary size history is tracked)
    
    * add snapshot for compact mode
    
    * snapshot for opening config editor and going to next tab to the right
    
    * snapshot for opening config editor and selecting a value to edit

--- tests/snapshots.rs
+++ tests/snapshots.rs
@@ -7,7 +7,11 @@ use insta::assert_snapshot;
 use mds_config::AppConfig;
 use mds_log::{LogLevel, prelude::Logger};
 use mds_tui::Model;
-use ratatui::{Terminal, backend::TestBackend};
+use ratatui::{
+    Terminal,
+    backend::TestBackend,
+    crossterm::event::{KeyCode, KeyEvent, KeyModifiers},
+};
 use semver::Version;
 
 const TEST_APP_VERSION: Version = Version::new(1, 2, 3);
@@ -32,6 +36,15 @@ fn test_render_default() {
 }
 
 #[test]
+fn test_render_compact_mode() {
+    let mut cfg = AppConfig::default();
+    cfg.ui.compact = true;
+    let model = setup_app(cfg);
+    let term = draw(model).unwrap();
+    assert_snapshot!(term.backend());
+}
+
+#[test]
 fn test_render_default_search_box() {
     let mut model = setup_app(AppConfig::default());
 
@@ -46,6 +59,33 @@ fn test_render_default_config_editor_box() {
     let mut model = setup_app(AppConfig::default());
 
     mds_tui::update(&mut model, mds_tui::Message::PopupConfig);
+
+    let term = draw(model).unwrap();
+    assert_snapshot!(term.backend());
+}
+
+#[test]
+fn test_render_default_config_editor_box_next_tab() {
+    let mut model = setup_app(AppConfig::default());
+
+    mds_tui::update(&mut model, mds_tui::Message::PopupConfig);
+    mds_tui::update(
+        &mut model,
+        mds_tui::Message::BoxInput(KeyEvent::new(KeyCode::Right, KeyModifiers::empty())),
+    );
+    let term = draw(model).unwrap();
+    assert_snapshot!(term.backend());
+}
+
+#[test]
+fn test_render_default_config_editor_box_select_edit() {
+    let mut model = setup_app(AppConfig::default());
+
+    mds_tui::update(&mut model, mds_tui::Message::PopupConfig);
+    mds_tui::update(
+        &mut model,
+        mds_tui::Message::BoxInput(KeyEvent::new(KeyCode::Enter, KeyModifiers::empty())),
+    );
 
     let term = draw(model).unwrap();
     assert_snapshot!(term.backend());
