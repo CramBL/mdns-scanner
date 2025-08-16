e02e82b184362e0bc08010f90d08e28100b47488
Marc Beck König <mbkj@tutamail.com>
2025-08-16 20:41:02 +0200

    avoid unnecessary constraint calculation

--- crates/mds-tui/src/table_pane/util.rs
+++ crates/mds-tui/src/table_pane/util.rs
@@ -5,7 +5,6 @@ pub(crate) struct ColumnConstraints {
     pub(crate) max_ip_len: u16,
     pub(crate) max_hostname_len: u16,
     pub(crate) max_packets_count_len: u16,
-    pub(crate) max_services_len: u16,
 }
 
 impl Default for ColumnConstraints {
@@ -14,7 +13,6 @@ impl Default for ColumnConstraints {
             max_ip_len: 10,
             max_hostname_len: 10,
             max_packets_count_len: 10,
-            max_services_len: 10,
         }
     }
 }
@@ -24,7 +22,6 @@ impl ColumnConstraints {
         let mut max_ip_len: u16 = 0;
         let mut max_hostname_len: u16 = 0;
         let mut max_packets_count_len: u16 = 0;
-        let mut max_services_len: u16 = 0;
 
         for it in items {
             let tmp_ip_len = it.ip.max_unicode_width();
@@ -41,18 +38,12 @@ impl ColumnConstraints {
             if max_packets_count_len < tmp_packets_count_len {
                 max_packets_count_len = tmp_packets_count_len;
             }
-
-            let tmp_services_len = it.max_service_instance_unicode_width();
-            if max_services_len < tmp_services_len {
-                max_services_len = tmp_services_len;
-            }
         }
 
         Self {
             max_ip_len,
             max_hostname_len,
             max_packets_count_len,
-            max_services_len,
         }
     }
 }
